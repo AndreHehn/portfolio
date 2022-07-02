d3e62b68fa7b6aaa0fa26486f15cda657cdc8bb4
Andre Hehn <a.hehn@web.de>
2022-07-02 20:29:59 +0200

    width changed. first @media defined. began contact

--- src/app/myworks/myworks.component.ts
+++ src/app/myworks/myworks.component.ts
@@ -9,19 +9,19 @@ export class MyworksComponent implements OnInit {
   projects = [
     {
       "name": "EL POLLO LOCO",
-      "text": "With this little jump'n'run game <br>i learned object oriented programming.",
+      "text": "With this little jump'n'run game i learned object oriented programming.",
       "src": "pollo.png",
       "direction" :"left"
     },
     {
       "name": "PORTFOLIO",
-      "text": "This was my first angular project.<br>While other projects were being coded,<br> this project grew and was constantly improved.",
+      "text": "This was my first angular project. While other projects were being coded, this project grew and was constantly improved.",
       "src": "portfolio.png",
       "direction" :"right"
     },
     {
       "name": "RING OF FIRE",
-      "text": "By programming this well-known drinking game,<br> I improved my skills in Angular,<br>especially creating animations and <br>working with databases(in this case firebase from google). ",
+      "text": "By programming this well-known drinking game, I improved my skills in Angular,especially creating animations and working with databases(in this case firebase from google). ",
       "src": "ringoffire.png",
       "direction" :"left"
     },
