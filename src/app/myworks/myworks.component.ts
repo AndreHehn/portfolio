4515480e456920bfef2a49c3c22da7d403943c98
Andre Hehn <a.hehn@web.de>
2022-07-11 17:59:39 +0200

    typos

--- src/app/myworks/myworks.component.ts
+++ src/app/myworks/myworks.component.ts
@@ -19,7 +19,7 @@ export class MyworksComponent implements OnInit {
       "name": "EL POLLO LOCO",
       "text": "This is little jump'n'run game, implemented with object-oriented programming (OOP). The used languages: JavaScript, CSS and HTML",
       "src": "pollo.png",
-      "direction": "left",
+      "direction": "right",
       "github": "https://github.com/AndreHehn/portfolio",
       "link": "https://andrehehn.de/el_pollo_loco/"
     },
@@ -27,13 +27,13 @@ export class MyworksComponent implements OnInit {
       "name": "PORTFOLIO",
       "text": "That was my first angular project. While working on other projects, this project grew and was constantly improved. The used languages: TypeScript, SCSS and HTML",
       "src": "portfolio.png",
-      "direction": "right",
+      "direction": "left",
       "github": "https://github.com/AndreHehn/el_pollo_loco",
       "link": "#"
     },
     {
       "name": "RING OF FIRE",
-      "text": "During programming this well-known drinking game, I improved my skills in Angular, especially creating animations and working with databases (in this case, firebase from google).",
+      "text": "During programming this well-known drinking game, I improved my skills in Angular, especially creating animations and working with databases (in this case, firebase from google). The used languages: TypeScript, SCSS and HTML",
       "src": "ringoffire.png",
       "direction": "right",
       "github": "https://github.com/AndreHehn/ringoffire",
